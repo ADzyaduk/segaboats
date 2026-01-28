287680e507249b32374ccc481696928345d81d9d
unknown <avdzyaduk@gmail.com>
2026-01-28 20:53:18 +0300

    ddd

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -27,7 +27,7 @@ export default defineNuxtConfig({
     telegramBotToken: '', // Будет прочитано из NUXT_TELEGRAM_BOT_TOKEN в production
     telegramWebhookSecret: '', // Будет прочитано из NUXT_TELEGRAM_WEBHOOK_SECRET в production
     telegramAdminChatId: process.env.TELEGRAM_ADMIN_CHAT_ID || process.env.NUXT_TELEGRAM_ADMIN_CHAT_ID || '', // Будет прочитано из TELEGRAM_ADMIN_CHAT_ID или NUXT_TELEGRAM_ADMIN_CHAT_ID
-    webhookUrl: process.env.WEBHOOK_URL || 'https://v-more.ru/webhook',
+    webhookUrl: process.env.WEBHOOK_URL || 'https://n8n.v-more.ru/webhook',
     webhookApiKey: process.env.WEBHOOK_API_KEY || '',
     adminPassword: process.env.ADMIN_PASSWORD || 'admin2026',
 
